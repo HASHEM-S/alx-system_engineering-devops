22726ea802b8b7437060a7200472dcb4be79c1b4
HASHEM-S <popleeswager077@gmail.com>
2024-04-20 00:08:19 -0700

    1-install_a_package.pp

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,8 @@
 #!/usr/bin/pup
 # Install a specific version of flask (2.1.0)
-package {'flask':
-	ensure => '2.1.0',
-	provider => 'pip'
+exec { 'install_flask':
+  command     => '/usr/bin/pip3 install Flask==2.1.0',
+  path        => ['/usr/bin', '/usr/local/bin'],
+  logoutput   => true,
+  unless      => '/usr/bin/pip3 show Flask | grep -q "Version: 2.1.0"',
 }
